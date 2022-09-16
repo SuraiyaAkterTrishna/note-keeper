cf448b28cd4a3f226656e31c8012cec5d8552a53
Suraiya  Akter Trishna <suraiyaaktertrishna@gmail.com>
2022-09-16 10:57:51 +0600

    'done'

--- src/firebase.js
+++ src/firebase.js
@@ -1,16 +1,17 @@
-import firebase from 'firebase/compat/app';
-import 'firebase/compat/firestore';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-
+// Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyCzm_kNY5QHNXwTv0_rqmOcyMOPRlh0jlA",
-    authDomain: "note-keep-62b77.firebaseapp.com",
-    projectId: "note-keep-62b77",
-    storageBucket: "note-keep-62b77.appspot.com",
-    messagingSenderId: "512619696866",
-    appId: "1:512619696866:web:ef793e289abce1f5d404bb"
+  apiKey: "AIzaSyCzm_kNY5QHNXwTv0_rqmOcyMOPRlh0jlA",
+  authDomain: "note-keep-62b77.firebaseapp.com",
+  projectId: "note-keep-62b77",
+  storageBucket: "note-keep-62b77.appspot.com",
+  messagingSenderId: "512619696866",
+  appId: "1:512619696866:web:ef793e289abce1f5d404bb"
 };
 
-firebase.initializeApp(firebaseConfig);
-
-export default firebase;
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
