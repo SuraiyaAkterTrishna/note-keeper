69d5eac79d2055bf8a49539ed45e8ac5cf040171
Suraiya  Akter Trishna <suraiyaaktertrishna@gmail.com>
2022-09-16 11:36:36 +0600

    'complete'

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 //components
 import Home from './components/Home';
-import DataProvider from './context/DataProvider';
 
 
 
 function App() {
   return (
-    <DataProvider>
       <Home />
-    </DataProvider>
   );
 }
 
